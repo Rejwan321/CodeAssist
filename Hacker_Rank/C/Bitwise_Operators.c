28c7f923d21f2ffefefc092b8db4f9cd8eda37ac
Rejwan Azam Mondal <88759448+Rejwan321@users.noreply.github.com>
2023-04-03 14:47:25 +0530

    Create Bitwise_Operators.c

---
+++ Hacker_Rank/C/Bitwise_Operators.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+
+void calculate_the_maximum(int n, int k) {
+  //Write your code here.
+}
+
+int main() {
+    int n, k;
+  
+    scanf("%d %d", &n, &k);
+    calculate_the_maximum(n, k);
+ 
+    return 0;
+}
