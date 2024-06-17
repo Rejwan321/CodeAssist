78d4c39019a6c8c173c219a54529a56062ba1dd7
Rejwan Azam Mondal <rejwan.azam.mondal321@gmail.com>
2024-06-17 23:58:48 +0530

    Create solution.java

---
+++ HackerRank/java/java 7/Java Stdin and Stdout/II/solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        double d = sc.nextDouble();
+        String s = sc.nextLine();
+        s = sc.nextLine();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
