778eb9b49795ad753bcbb916f771c17b2a8f601d
Rejwan Azam Mondal <rejwan.azam.mondal321@gmail.com>
2024-06-20 23:34:50 +0530

    Create solution.java

---
+++ HackerRank/java/java 7/Java End-of-file/solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 1 ;
+        while(sc.hasNext()){
+        System.out.println(n+" "+sc.nextLine());
+        n++;
+        }
+        sc.close();
+    }
+}
